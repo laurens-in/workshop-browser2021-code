3795c2c223f58b079d623652a64825be9717f5b6
laurens <laurens.inauen@hkb.bfh.ch>
2021-03-20 22:25:57 +0100

    test

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -4,7 +4,16 @@ const countBack = x => {
     countBack(x - 1)                    // call the function recursively
 }
 
+const countOtherBack = x => {
+    if (x !== 0) {
+        console.log(x)
+        countBack(x - 1);
+    }; // simple stop condition
+    console.log(x)                      // log x to console                        // call the function recursively
+}
+
 countBack(10)
+countBackOther(12)
 
 const synth = new Tone.Synth().toDestination();
 
