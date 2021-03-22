bb47647f23ef58e54d0c30b5f103c6190c7095c2
laurens <laurens.inauen@hkb.bfh.ch>
2021-03-22 21:53:22 +0100

    add small examples

---
+++ small-examples.js
@@ -0,0 +1,199 @@
+// sounds from VSCO Community Library
+class SimpleSampler {
+
+    p1;
+    p2;
+    p3;
+    p4;
+    output;
+
+    constructor() {
+        this.output = new Tone.Gain(0.7).toDestination()
+
+
+        this.p1 = new Tone.Sampler({
+            urls: {
+                'C3': "./sounds/C3_1.wav",
+                'G4': "./sounds/G4_1.wav"
+            }
+        }).connect(new Tone.Gain(0.7)).connect(this.output);
+
+        this.p2 = new Tone.Sampler({
+            urls: {
+                'C3': "./sounds/C3_2.wav",
+                'G4': "./sounds/G4_2.wav"
+            }
+        }).connect(new Tone.Gain(0.7)).connect(this.output);
+
+        this.p3 = new Tone.Sampler({
+            urls: {
+                'G3': "./sounds/G3_1.wav",
+                'C4': "./sounds/C4_1.wav"
+            }
+        }).connect(new Tone.Gain(0.7)).connect(this.output);
+
+        this.p4 = new Tone.Sampler({
+            urls: {
+                'G3': "./sounds/G3_2.wav",
+                'C4': "./sounds/C4_2.wav"
+            }
+        }).connect(new Tone.Gain(0.7)).connect(this.output);
+
+
+    }
+}
+
+class SimplePlayer {
+
+    currentIndex = 0;
+    playingNote = 0;
+
+
+    constructor(sampler, start = 0) {
+        this.sampler = sampler;
+        this.currentIndex = start;
+    }
+
+    play = (note, length, time, velocity) => {
+        if (note == this.playingNote) {
+            this.currentIndex += 1;
+        }
+        let currentCount = this.currentIndex % 4;
+
+        switch (Math.floor(Math.random() * 4)) {
+            case 0:
+                this.sampler.p1.triggerAttackRelease(note, length, time, velocity);
+                break;
+
+            case 1:
+                this.sampler.p2.triggerAttackRelease(note, length, time, velocity);
+                break;
+
+            case 2:
+                this.sampler.p3.triggerAttackRelease(note, length, time, velocity);
+                break;
+
+            case 3:
+                this.sampler.p4.triggerAttackRelease(note, length, time, velocity);
+                break;
+
+        }
+        this.playingNote = note;
+    };
+}
+
+// first we make a little sampler using Tone.js
+const sampler = new SimplePlayer(new SimpleSampler());
+
+
+// Recrusion - Example 1
+
+const countBack = (x) => {
+    if (x >= 0) return console.log(0); // stopping condition
+    console.log(x);                    // instructions
+    countBack(x - 1);                    // recursive call
+}
+
+// Recursion - Example 2
+
+const playRecursive = (note, length) => {
+    if (length > 5000) return sampler.play(note);
+    sampler.play(note);
+    audioContextTimers.setTimeout(() => playRecursive(note, length * 2), length)
+}
+
+
+// Recursion - Example 3
+
+// HELPER FUNCTION
+const playSample = (note) => {
+    sampler.play(note)
+    return new Promise((resolve) => audioContextTimers.setTimeout(resolve, 1000))
+}
+
+class Figure {
+    notes;
+    variation;
+
+    constructor(notes) {
+        this.notes = notes
+    }
+
+    next = () => {
+        this.variation = new Figure(this.notes.reverse().concat(this.notes.slice(2)))
+    }
+
+    playFigure = async () => {
+        for (const note of this.notes) {
+            await playSample(note);
+        }
+    }
+
+    playSample = (note) => {
+        sampler.play(note)
+        return new Promise((resolve) => audioContextTimers.setTimeout(resolve, 1000))
+    }
+}
+
+const notes = ['C4', 'D4', 'D#4']
+
+const someFigure = new Figure(notes);
+
+// button 1
+// someFigure.playFigure();
+
+// someFigure.next();
+
+//someFigure.variation.playFigure();
+
+
+// Representation of Musical Data
+
+// This part in the markdown file is just pseudocode, nothing to see here :)
+
+// if you are interested in the cartesian function i'm not gonna disappoint you:
+
+const cartesian =
+    (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+
+// got it form here: https://stackoverflow.com/questions/12303989/cartesian-product-of-multiple-arrays-in-javascript
+
+
+
+const someMusicalData = [66, 23, 76, 89]
+
+const someOtherData = [[120, 500], [330, 800]]
+
+
+const pitches = [100, 200]
+const lengths = [5, 7, 8]
+
+cartesian(notes, pitches)
+
+[
+    [100, 5],
+    [100, 7],
+    [100, 8],
+    [200, 5],
+    [200, 7],
+    [200, 8]
+]
+
+
+class someStructure {
+    pitches
+    nextSequence;
+
+    constructor(pitches, sequence) {
+        this.pitches = pitches
+        this.nextSequence = sequence
+
+    }
+}
+
+// some TypeScript maybe? some PureScript?
+
+
+
+
+
---
+++ small-examples.md
@@ -0,0 +1,45 @@
+---
+layout: page
+title: Composition As Code
+permalink: /composition-as-code
+---
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.13/Tone.js" integrity="sha512-SAB2YrHeaZfb6W1w+tAMm+IUICzUMyf7TJ8upY+NjLYl8jseufUW4yYzoSHfNL9N2rzDlw5PWJrf7rPIQ6VhNw==" crossorigin="anonymous"></script>
+<script src="./bundled-libraries/z-pattern-matching.js"></script>
+<script src="./bundled-libraries/audio-context-timers-bundle.js"></script>
+<script src="small-examples.js"></script>
+
+I will now try to show a how we can use concepts from programming in a way that allows us to think differently about music composition.
+
+# Recursion
+
+Recursion is a powerful concept in programming. Its essentially a process which calls itself in its own definition.
+
+```js
+const countBack = (x) => {
+    if (x >= 0) return console.log(0); // stopping condition
+    console.log(x);                    // instructions
+    countBack(x-1);                    // recursive call
+}
+
+countBack(10)
+```
+<button id="play">Run</button>
+<script>
+document.getElementById("play").addEventListener("click", () => countBack(10));
+</script>
+
+We can use recursion is essentially a means to structure our code and hence our composition. We can think about structuring time recursively:
+
+```js
+const playRecursive = (note, length) => {
+    if (length > 5000) return sampler.play(note);
+    sampler.play(note);
+    audioContextTimers.setTimeout(() => playRecursive(note, length * 2), length)
+}
+
+playRecursive('C4', 20)
+```
+<button id="play">Run</button>
+<script>
+document.getElementById("play").addEventListener("click", () => playRecursive('C4', 20));
+</script>
